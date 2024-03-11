4fd709fc7d7c4f6cdff19f653a33be836cae3607
Sharvani <130580948+01FE22BEC237@users.noreply.github.com>
2024-03-11 22:07:31 +0530

    Add files via upload

---
+++ Km_miles.c
@@ -0,0 +1,12 @@
+/*program to compute km to miles*/
+# include <stdio.h>
+int main()
+{
+float miles;
+float kilometre;
+printf("Enter the miles\n");
+scanf("%f",&miles);
+kilometre=1.6*miles;
+printf("%f",kilometre);
+
+}
