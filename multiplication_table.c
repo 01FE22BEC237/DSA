a427e74dad1a5beffc611c74363beddb2655b4d8
Sharvani <130580948+01FE22BEC237@users.noreply.github.com>
2024-03-11 21:50:00 +0530

    Add files via upload

---
+++ multiplication_table.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+ void G_multification(int n)
+ { int i;
+   for(i=1;i<=10;i++)
+   {
+    printf("%d * %d = %d\n",n,i,n*i);
+   }
+  return ;
+ }
+ int main()
+ {
+ int n;
+ printf("Enter the number ");
+ scanf("%d",&n);
+ G_multification(n);
+ return 0;
+ }
