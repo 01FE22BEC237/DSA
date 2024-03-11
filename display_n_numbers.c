08622fb060a46f8c165987037bf39aaf338ec8d9
Sharvani <130580948+01FE22BEC237@users.noreply.github.com>
2024-03-11 22:20:12 +0530

    Add files via upload

---
+++ display_n_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ void display(int n)
+ {
+ int i=1;
+ while (i<=n)
+ {
+  printf("%d ",i);
+  i=i+1;
+ } //end of while
+ return ;
+ }
+ int main()
+ {
+ int n;
+ printf ("Enter the N value ");
+ scanf("%d",&n);
+ display(n);
+ return 0;
+ }
