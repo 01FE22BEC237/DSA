9809d2f14e3a46fa2194cc4f6e7e36b32c1dee32
Sharvani <130580948+01FE22BEC237@users.noreply.github.com>
2024-03-11 21:39:31 +0530

    Add files via upload

---
+++ positive_negative.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+ int x;
+ printf("Enter the number \n");
+ scanf("%d",&x);
+ if(x>0)
+ {
+ printf("x is positive");
+ }
+ else
+ {
+  printf("x is negative");
+  }
+return 0;
+}
